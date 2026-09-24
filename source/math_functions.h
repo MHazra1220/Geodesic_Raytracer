1adec2d7306cc8fa47bdca775ea95ef12bac82ae
poq <madhuwrithazra1220@gmail.com>
2026-09-24 03:45:50 +0100

    Separate maths functions into a different file.

---
+++ source/math_functions.h
@@ -0,0 +1,24 @@
+#ifndef MATH_FUNCTIONS
+#define MATH_FUNCTIONS
+
+#include "utilities/float_defn.h"
+
+// Quaternionic arithmetic functions.
+
+__host__ __device__ void crossProduct(Real const u[3], Real const v[3], Real cross[3]);
+// Calculate the Hamilton (quaternionic) product of two quaternions.
+__host__ __device__ void quatProduct(Real const u[4], Real const v[4], Real result[4]);
+// Rotates a 3D Cartesian vector, vec (a pure quaternion), by rotation_quat.
+// result will be the rotated vector represented as a pure quaternion.
+__host__ __device__ void rotateVecByQuat(Real vec[4], Real rotation_quat[4], Real result[4]);
+
+Real rMagnitude(Real const r[3]);
+__device__ Real rMagnitudeDev(Real const r[3]);
+__device__ Real rInvMagnitudeDev(Real const r[3]);
+__host__ __device__ Real rSquared(Real const r[3]);
+
+// Calculates the scalar product of a velocity with in some metric.
+__host__ __device__ Real scalarProduct(Real const v[4], Real const g[4][4]);
+__host__ __device__ void invertSymmetric4Metric(Real const m[4][4], Real m_inv[4][4]);
+
+#endif
--- source/trace_kernel_utils_CPU.h
+++ source/trace_kernel_utils_CPU.h
@@ -2,7 +2,6 @@
 #define TRACE_KERNEL_UTILS
 
 #include "utilities/float_defn.h"
-#include "math_functions.h"
 
 // I hate having so many things here, but CUDA doesn't like modularised code; it's easier
 // to put everything the raytracing kernel uses in a single place.
@@ -13,6 +12,24 @@
 // pi is needed by the host when importing sky maps and used by the device when getting pixels.
 Real const pi_host = 3.141592653589793;
 __device__ __constant__ Real pi_device = 3.141592653589793;
+
+// Quaternionic arithmetic functions.
+
+__host__ __device__ void crossProduct(Real const u[3], Real const v[3], Real cross[3]);
+// Calculate the Hamilton (quaternionic) product of two quaternions.
+__host__ __device__ void quatProduct(Real const u[4], Real const v[4], Real result[4]);
+// Rotates a 3D Cartesian vector, vec (a pure quaternion), by rotation_quat.
+// result will be the rotated vector represented as a pure quaternion.
+__host__ __device__ void rotateVecByQuat(Real vec[4], Real rotation_quat[4], Real result[4]);
+
+Real rMagnitude(Real const r[3]);
+__device__ Real rMagnitudeDev(Real const r[3]);
+__device__ Real rInvMagnitudeDev(Real const r[3]);
+__host__ __device__ Real rSquared(Real const r[3]);
+
+// Calculates the scalar product of a velocity with in some metric.
+__host__ __device__ Real scalarProduct(Real const v[4], Real const g[4][4]);
+__host__ __device__ void invertSymmetric4Metric(Real const m[4][4], Real m_inv[4][4]);
 
 class Schwarzschild
 {
